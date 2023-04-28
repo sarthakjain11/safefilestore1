1798b1585709757283f170477ea0531808e66797
sarthakjain11 <78959720+sarthakjain11@users.noreply.github.com>
2023-04-28 16:20:05 +0530

    update

--- connection_secret.py
+++ connection_secret.py
@@ -1,5 +1,5 @@
 # Azure Blob Storage connection string
-conn_str = 'DefaultEndpointsProtocol=https;AccountName=grp2ccproject;AccountKey=IsXNMGKSYADQ339guvpmOeNMgUfC6CJkEZpJy98Rn6UFVqpiIrSfyrbp0eRoJ+dYmqztWLzgRt9K+AStqb4MQQ==;EndpointSuffix=core.windows.net'
+conn_str = 'AZURE-CONNECTION-STRING'
 
 # Secret key for Flask session management
-secret_key = 'cc670082c8ab2d68de14221e78983278'
+secret_key = 'SECRET_KEY'
